45406ca37440f73ebeae3c7d9e1a4e598531e11c
karenalves <karen.alves@sga.pucminas.br>
2018-09-10 09:17:29 -0300

    Criação de Entidades

---
+++ WikiTutorial.Core/Entities/ProductEntity/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiTutorial.Entities.ProductEntity
+{
+    public class Product : FullAuditedEntity<long>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public float Value { get; set; }
+
+        public Product()
+        {
+            this.CreationTime = DateTime.Now;
+        }
+
+        public Product(string name, string desc, float value)
+        {
+            this.CreationTime = DateTime.Now;
+            this.Name         = name;
+            this.Description  = desc;
+            this.Value        = value;
+        }
+    }
+}
